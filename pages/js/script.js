12711376d824770c2ebea9c449a70110cae2deee
Mirvokhid-group <50291853+Mirvokhid-group@users.noreply.github.com>
2020-04-25 23:43:11 +0500

    Delete script.js

--- pages/js/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('myslide').onmousemove = function (event) {
-	var x = event.offsetX; //releted to parent
-	console.log(x);
-	document.getElementById('two').style.width = x + 'px'
-}
-
-// document.getElementById('myslide').onmouseleave = function (event) {
-// 	document.getElementById('two').style.width = '640px';
-// }
